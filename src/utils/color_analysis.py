b8fcd4e9560cbe108dd834ac877b5253bcf2aa3b
Ali Jamali <ali292jamali@gmail.com>
2026-05-01 14:18:13 +0100

    Some bugs and errors fixed and pipeline tested

--- src/utils/color_analysis.py
+++ src/utils/color_analysis.py
@@ -6,7 +6,7 @@ def extract_dominant_colors(image, k=3):
     image = image.resize((150, 150))  # speed
     pixels = np.array(image).reshape(-1, 3)
 
-    kmeans = KMeans(n_clusters=k, n_init=10)
+    kmeans = KMeans(n_clusters=k, n_init=10, random_state=42)
     kmeans.fit(pixels)
 
     colors = kmeans.cluster_centers_.astype(int)
