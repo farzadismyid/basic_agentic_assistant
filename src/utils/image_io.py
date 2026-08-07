6dcb7f1a98e10a61322d706b4d4a5e5c3265c81c
Ali “Farzad” Jamali <ali292jamali@gmail.com>
2026-08-08 00:52:40 +0100

    Clean up image_io.py

--- src/utils/image_io.py
+++ src/utils/image_io.py
@@ -4,15 +4,3 @@ from PIL import Image
 def load_image(path: str) -> Image.Image:
     image = Image.open(path).convert("RGB")
     return image
-
-
-    colors = kmeans.cluster_centers_.astype(int)
-    return colors
-
-
-def rgb_to_hex(color):
-    return '#%02x%02x%02x' % tuple(color)
-
-
-def get_palette_hex(colors):
-    return [rgb_to_hex(c) for c in colors]
