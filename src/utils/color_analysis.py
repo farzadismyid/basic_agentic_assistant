d77c613ed174f57e667ac3cdaaffc2d202bbe69c
Ali “Farzad” Jamali <ali292jamali@gmail.com>
2026-08-08 00:51:48 +0100

    Add functions to convert RGB colors to HEX

--- src/utils/color_analysis.py
+++ src/utils/color_analysis.py
@@ -9,3 +9,13 @@ def extract_dominant_colors(image, k=3):
     kmeans = KMeans(n_clusters=k, n_init=10, random_state=42)
     kmeans.fit(pixels)
 
+    colors = kmeans.cluster_centers_.astype(int)
+    return colors
+
+
+def rgb_to_hex(color):
+    return '#%02x%02x%02x' % tuple(color)
+
+
+def get_palette_hex(colors):
+    return [rgb_to_hex(c) for c in colors]
