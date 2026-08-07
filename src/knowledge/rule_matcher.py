c6308819876596960b28e85bafbacb80094c22e7
Ali “Farzad” Jamali <ali292jamali@gmail.com>
2026-08-08 00:50:44 +0100

    Delete find_best_match function

--- src/knowledge/rule_matcher.py
+++ src/knowledge/rule_matcher.py
@@ -1,16 +1,1 @@
-def find_best_match(detected_colors, rules):
-    # naive approach: pick first detected color
-    base_color = detected_colors[0].lower()
 
-    if base_color in rules:
-        return {
-            "base_color": base_color,
-            "matches": rules[base_color]["matches"],
-            "style": rules[base_color]["style"]
-        }
-
-    return {
-        "base_color": base_color,
-        "matches": [],
-        "style": "No rule found for this color."
-    }
